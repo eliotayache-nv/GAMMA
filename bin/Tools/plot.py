365eb18a6ab302294b2492c2dd04f5af554dd089
Eliot Ayache <eliot.ayache@gmail.com>
2020-12-08 21:15:50 +0000

    [ADDED] slick function in 2D plot

--- bin/Tools/plot.py
+++ bin/Tools/plot.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author: eliotayache
 # @Date:   2020-05-14 16:24:48
-# @Last Modified by:   eliotayache
-# @Last Modified time: 2020-11-26 14:38:00
+# @Last Modified by:   Eliot Ayache
+# @Last Modified time: 2020-12-08 15:18:59
 
 
 import numpy as np
@@ -11,17 +11,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.transforms
 from matplotlib.colors import LogNorm
+import matplotlib.gridspec as gridspec
 import matplotlib.cm as cm
 import glob
 import os
 import string
 import glob
+from isentropic import *
 
 # plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-plt.rc('xtick', labelsize='x-large')
-plt.rc('ytick', labelsize='x-large')
-plt.rcParams['savefig.dpi'] = 200
+plt.rc('font', family='serif', size=12)
+plt.rc('xtick', labelsize=12) 
+plt.rc('ytick', labelsize=12) 
+plt.rc('legend', fontsize=12) 
+plt.rcParams['savefig.dpi'] = 300
 
 
 def plot(key, it, contour=False):
@@ -64,17 +67,43 @@ def readData(key, it=None, sequence=False):
   return(data)
 
 
+
 def getArray(data, key):
   return(data.pivot(index='j', columns='i', values=key).to_numpy())
 
 
+
+def plotMulti(data, keys, log=[], labels={}, **kwargs):
+
+  Nk = len(keys)
+  f, axes = plt.subplots(Nk, 1, sharex=True, figsize=(6,2*Nk))
+
+  for key,k,ax in zip(keys,range(Nk),axes):
+
+    logkey = False
+    label = None
+    if key in log:
+      logkey = True
+    if key in labels:
+      label = labels[key]
+
+    plot1D(data, key, ax, log=logkey, label=label, **kwargs)
+
+  plt.tight_layout()
+
+  return(f, axes)
+
+
+
 def plot1D(data, key, 
+  ax = None,
   mov="x",
   log=False, 
   v1min=None,
-  color=None, 
-  r2=False, 
-  trac=None):
+  tracer=True,
+  line=True,
+  r2=False,
+  label=None):
 
   if key=="lfac":
     var = "vx"
@@ -100,14 +129,23 @@ def plot1D(data, key,
   if v1min:
     vmin = v1min
 
-  f = plt.figure()
+  if ax == None:
+    f = plt.figure()
+    ax = plt.gca()
+
+  if label is not(None):
+    ax.set_ylabel(label)
+  else:
+    ax.set_ylabel(key)
 
   if log==True:
-    plt.yscale('log')
+    ax.set_yscale('log')
 
-  plt.plot(x,z,'k',zorder=1)
-  plt.scatter(x,z, c='None', edgecolors='k', lw=2, zorder=2)
-  plt.scatter(x,z, c=tracvals, edgecolors='None', zorder=3)
+  if line:
+    ax.plot(x,z,'k',zorder=1)    
+  ax.scatter(x,z, c='None', edgecolors='k', lw=2, zorder=2, label="numerical")
+  if tracer:
+    ax.scatter(x,z, c=tracvals, edgecolors='None', zorder=3, cmap='cividis')
 
 
 
@@ -125,7 +163,13 @@ def quadMesh(data, key,
   r2=False,
   cmap='magma',
   tlayout=True,
-  colorbar=True):
+  colorbar=True,
+  slick=False,
+  expand=False):
+
+  if key2 and geometry!="polar":
+    print("Use polar geometry with key2")
+    return
 
   z = data.pivot(index='j', columns='i', values=key).to_numpy()
   x  = data.pivot(index='j', columns='i', values='x').to_numpy()
@@ -164,11 +208,16 @@ def quadMesh(data, key,
   if v2min:
     vmin2 = v2min
 
-  if (key2):
-    f, (ax2, ax) = plt.subplots(1,2, sharey=True)
+  f = plt.figure()
+  ax = plt.axes(projection="polar")
+  ax.set_thetamax(ymax*180./np.pi)
+  if key2:
+    ax.set_thetamin(-ymax*180./np.pi)
   else:
-    f = plt.figure()
-    ax = plt.gca()
+    ax.set_thetamin(ymin*180./np.pi)
+
+  if slick:
+    ax.axis("off")
 
   for j in range(z.shape[0]-1):
     xj = x - dx/2.
@@ -184,11 +233,11 @@ def quadMesh(data, key,
     xj[j+1,:] = xj[j,:]   #these xj[] might not exist
     yj[j+1,:] = yj[j,:]+dyj[j,:]   #these xj[] might not exist
 
-    if (geometry=='polar'):
-      xx = xj * np.cos(yj)
-      yy = xj * np.sin(yj)
-      xj = xx
-      yj = yy
+    # if (geometry=='polar'):
+    #   xx = xj * np.cos(yj)
+    #   yy = xj * np.sin(yj)
+    #   xj = xx
+    #   yj = yy
 
 
     mask = np.zeros(z.shape)+1
@@ -213,36 +262,35 @@ def quadMesh(data, key,
 
     if key2:  
       if log2==True:
-        im2 = ax2.pcolor(-yj, xj, zj2, 
+        im2 = ax.pcolor(-yj, xj, zj2, 
           norm=LogNorm(vmin=vmin2, vmax=vmax2), 
           edgecolors=edges,
           cmap=cmap,
           facecolor=color)
       else:
-        im2 = ax2.pcolor(-yj, xj, zj2, 
+        im2 = ax.pcolor(-yj, xj, zj2, 
           vmin=vmin2, vmax=vmax2, 
           edgecolors=edges, 
           cmap=cmap,
           facecolor=color)
 
 
-  if (quiver):
-    if (geometry=='polar'):
-      xx = x * np.cos(y)
-      yy = x * np.sin(y)
-      u = vx * np.cos(y)
-      v = vx * np.sin(y)
-      q = plt.quiver(yy[:,::5],xx[:,::5],v[:,::5],u[:,::5], headwidth=10, headlength=10)
+  # if (quiver):
+    # if (geometry=='polar'):
+    #   xx = x * np.cos(y)
+    #   yy = x * np.sin(y)
+    #   u = vx * np.cos(y)
+    #   v = vx * np.sin(y)
+    #   q = plt.quiver(yy[:,::5],xx[:,::5],v[:,::5],u[:,::5], headwidth=10, headlength=10)
 
-  ax.set_aspect('equal')
-  if key2:
-    ax2.set_aspect('equal')
+  if geometry != "polar":
+    ax.set_aspect('equal')
 
   if colorbar:
     cb = f.colorbar(im, ax=ax, orientation='horizontal')
     cb.set_label(key, fontsize=18)
     if key2:
-      cb2 = f.colorbar(im2, ax=ax2, orientation='horizontal')
+      cb2 = f.colorbar(im2, ax=ax, orientation='horizontal')
       cb2.set_label(key2, fontsize=18)
 
   if tlayout:
@@ -261,6 +309,24 @@ def loopFigs(dir, key, oneDimensional = False, **kwargs):
       quadMesh(data, key, **kwargs)
     plt.savefig("../../results/%s/figs/%s.png" %(dir,filename.rstrip(".out").split(dir)[1]))
     plt.close()
+
+
+
+# ----------------------------------------------------------------------------------------
+# Specific functions
+def isenwave(data):
+  time = data["t"][0]
+  f, axes = plotMulti(data, ["rho","p","vx"], 
+    tracer=False, 
+    line=False, 
+    labels={"rho":"$\\rho$", "vx":"v"})
+  plotIsen1D(time, "rho", ax=axes[0], color="r", label="exact")
+  plotIsen1D(time, "p", ax=axes[1], color="r")
+  plotIsen1D(time, "vx", ax=axes[2], color="r")
+  plt.xlabel("x")
+  axes[0].legend()
+  plt.tight_layout()
+  return(f, axes)
 
 
 
