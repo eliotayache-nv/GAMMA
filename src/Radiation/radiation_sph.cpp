c91f576227fc09b57e6780ce2da691effe113e86
Eliot Ayache <eliot.ayache@gmail.com>
2021-01-28 13:25:08 +0000

    [FIX] The whole gammaMax tracer is advected, so it has to be multiplied by D too before advection. Exponent is back to 4/3

--- src/Radiation/radiation_sph.cpp
+++ src/Radiation/radiation_sph.cpp
@@ -2,7 +2,7 @@
 * @Author: Eliot Ayache
 * @Date:   2020-10-25 10:19:37
 * @Last Modified by:   Eliot Ayache
-* @Last Modified time: 2021-01-28 11:11:28
+* @Last Modified time: 2021-01-28 13:24:58
 */
 
 
@@ -159,8 +159,7 @@
     double lfac = S.lfac();
     double rho = S.prim[RHO];
 
-    // if (gme != 0.) return(lfac * pow(rho, 4./3.) / gme);
-    if (gme != 0.) return(lfac * pow(rho, 1./3.) / gme);
+    if (gme != 0.) return(lfac * pow(rho, 4./3.) / gme);
     else return(0);
 
   }
@@ -171,8 +170,7 @@
     double lfac = S.lfac();
     double rho = S.prim[RHO];
 
-    // if (gme_trac != 0.) return(lfac * pow(rho, 4./3.) / gme_trac);
-    if (gme_trac != 0.) return(lfac * pow(rho, 1./3.) / gme_trac);
+    if (gme_trac != 0.) return(lfac * pow(rho, 4./3.) / gme_trac);
     else return(0);
 
   }
@@ -193,8 +191,7 @@
 
     double lfac = S.lfac();
     double rho = S.prim[RHO];
-    // double lim = lfac * pow(rho, 4./3.);  // equiv. gammae = 1.
-    double lim = lfac * pow(rho, 1./3.);  // equiv. gammae = 1.
+    double lim = lfac * pow(rho, 4./3.);  // equiv. gammae = 1.
     S.prim[GMN] = lim / (lfac*rho);
     S.prim[GMX] = lim / (lfac*rho);
     S.cons[GMN] = lim;
@@ -206,8 +203,7 @@
 
     double lfac = S.lfac();
     double rho = S.prim[RHO];
-    // double lim = lfac * pow(rho, 4./3.) / (lfac*rho);  // equiv. gammae = 1.
-    double lim = lfac * pow(rho, 1./3.) / (lfac*rho);  // equiv. gammae = 1.
+    double lim = lfac * pow(rho, 4./3.) / (lfac*rho);  // equiv. gammae = 1.
     double *gmax = &S.prim[GMX];
     double *gmin = &S.prim[GMN];
 
