135da0d4fee45472163e362480c7457b71236b0e
Eliot Ayache <eliot.ayache@gmail.com>
2021-01-28 10:13:49 +0000

    [ADDED] calculation of gmax profile in BM.py

--- bin/Tools/BM.py
+++ bin/Tools/BM.py
@@ -2,7 +2,7 @@
 # @Author: Eliot Ayache
 # @Date:   2019-04-09 11:22:46
 # @Last Modified by:   Eliot Ayache
-# @Last Modified time: 2021-01-22 15:27:17
+# @Last Modified time: 2021-01-27 17:59:02
 
 # -*- coding: utf-8 -*-
 # @Author: Eliot Ayache
@@ -17,15 +17,24 @@ import matplotlib.pyplot as plt
 
 # ---------------------------------------------------------------------------------------
 
-mp_ = 1.6726219e-24
-c_  = 2.99792458e10
-GAMMA_ = (4./3.)
+c_      = 2.99792458e10   # speed of light (cm.s-1)
+mp_     = 1.6726219e-24   # Proton mass (g)
+me_     = 9.1093835e-28   # electron mass (g)
+sigmaT_ = 6.65345871e-25  # Thomson Cross section (cm2)
+GAMMA_  = (4./3.)
 
 BM_p_rho_ratio_ = 1.e-5    # ratio between p and rho in ISM
 E0 = 1.e53
 n0 = 1.e0
 rho0 = n0*mp_
 
+# Emissivity parameters
+p = 2.3
+eps_B_ = 0.1
+eps_e_ = 0.1
+zeta_ = 0.1
+
+# Normalisation
 lNorm = c_
 vNorm = c_
 rhoNorm = rho0
@@ -33,12 +42,6 @@ pNorm = rhoNorm * vNorm * vNorm
 
 
 # ---------------------------------------------------------------------------------------
-
-
-# TODO!
-# Careful, c = 1 in all the calculations
-
-
 class State(object):
   """docstring for State"""
   def __init__(self):
@@ -54,6 +57,8 @@ class State(object):
 
     self.lfac = 0
     self.h    = 0
+
+    self.gmax = 0
 
   def prim2aux(self):
     self.lfac = np.sqrt(1. / (1. - self.v**2 / c_**2))
@@ -75,8 +80,6 @@ class BM(object):
                 / (8. * np.pi * self.rho_ext * self.t**3 * c_**5)
       # Blandford&Mckee(1976) eq. 69
     self.lfacShock = np.sqrt(self.lfacShock_sqrd)
-
-    # print("lfacShock = %f, time = %f") %(self.lfacShock, self.t)
 
     self.RShock = self.t * (1. - 1. / (1. + 8. * self.lfacShock_sqrd)) * c_
 
@@ -109,6 +112,7 @@ class BM(object):
       S.D = self.Sa.rho
       S.p = self.Sa.p
       S.rho = S.D/S.lfac
+      S.gmax = 1.
 
     else:
       chi = (1. + 2. * (4.-self.k) * self.lfacShock_sqrd) * ( 1. - r / (c_ * self.t))
@@ -116,6 +120,17 @@ class BM(object):
       S.lfac = np.sqrt(self.Sf.lfac**2 / chi + 1)
       S.D = self.Sf.D * chi**(-7./4.) 
       S.rho = S.D/S.lfac
+
+      t0 = self.t/chi**(1./4.)
+      p0 = self.Sf.p
+      rho0 = self.Sf.rho
+      h0 = c_**2 + p0*GAMMA_/(GAMMA_-1.)/rho0
+      eps0 = rho0*(h0 - c_**2) / GAMMA_
+      eB0 = eps_B_ *eps0
+      B0 = np.sqrt(8.*np.pi*eB0)
+      S.gmax = 2.*19.*np.pi*me_*c_*self.Sf.lfac / (sigmaT_*B0**2*t0) \
+        * chi**(25./24.) / (chi**(19./12.)-1.)
+      print(S.gmax)
 
     return(S)
 
@@ -135,6 +150,8 @@ def plotBM1D(data, key, ax = None, **kwargs):
       y[i] = BW.local(x[i]).lfac
     if key == "p":
       y[i] = BW.local(x[i]).p/pNorm
+    if key == "gmax":
+      y[i] = BW.local(x[i]).gmax
 
   if ax is None:
     ax = plt.gca()
